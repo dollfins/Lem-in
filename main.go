0b6b13d24fb28e856d64fb3cc455c52879a5542d
jabdulsal <abdulsalamjamiu770@gmail.com>
2026-05-20 13:02:03 +0100

    new

--- main.go
+++ main.go
@@ -9,177 +9,263 @@ import (
 	"strings"
 )
 
-type Path []string
 type Ant struct {
 	ID, Pos int
-	Path    Path
+	Path    []string
 }
 
-func add(g map[string][]string, a, b string) {
+type Path struct {
+	Rooms []string
+	Ants  int
+}
+
+func addEdge(g map[string][]string, a, b string) {
 	g[a] = append(g[a], b)
 	g[b] = append(g[b], a)
 }
 
-func dfs(g map[string][]string, v, end string, vis map[string]bool, p Path, out *[]Path) {
-	vis[v] = true
-	p = append(p, v)
-	if v == end {
-		*out = append(*out, append(Path{}, p...))
-		vis[v] = false
-		return
-	}
-	for _, n := range g[v] {
-		if !vis[n] {
-			dfs(g, n, end, vis, p, out)
-		}
-	}
-	vis[v] = false
-}
-
-func interceptPath(a, b Path) bool {
-	m := map[string]bool{}
-	for i := 1; i < len(a)-1; i++ {
-		m[a[i]] = true
-	}
-	for i := 1; i < len(b)-1; i++ {
-		if m[b[i]] {
+func contains(s []string, t string) bool {
+	for _, v := range s {
+		if v == t {
 			return true
 		}
 	}
 	return false
 }
 
-func pathSelection(ps []Path) []Path {
-	sort.Slice(ps, func(i, j int) bool { return len(ps[i]) < len(ps[j]) })
-	var r []Path
-	for _, p := range ps {
+func dfs(g map[string][]string, start, end string, vis map[string]bool, path []string, paths *[][]string) {
+	vis[start] = true
+	path = append(path, start)
+
+	if start == end {
+		*paths = append(*paths, append([]string{}, path...))
+		vis[start] = false
+		return
+	}
+
+	for _, n := range g[start] {
+		if !vis[n] {
+			dfs(g, n, end, vis, path, paths)
+		}
+	}
+
+	vis[start] = false
+}
+
+func interceptPath(a, b []string) bool {
+	for i := 1; i < len(a)-1; i++ {
+		if contains(b[1:len(b)-1], a[i]) {
+			return true
+		}
+	}
+	return false
+}
+
+func turns(p []Path, ants int) int {
+	t := make([]Path, len(p))
+
+	for i := 0; i < ants; i++ {
+		b := 0
+
+		for j := range t {
+			if len(t[j].Rooms)+t[j].Ants < len(t[b].Rooms)+t[b].Ants {
+				b = j
+			}
+		}
+
+		t[b].Ants++
+	}
+
+	m := 0
+
+	for _, v := range t {
+		if v.Ants == 0 {
+			continue
+		}
+
+		x := len(v.Rooms) + v.Ants - 2
+
+		if x > m {
+			m = x
+		}
+	}
+	return m
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Correct format: go run (filename) (textfile)")
+		return
+	}
+	filePath := os.Args[1]
+	file, err := os.ReadFile(filePath + ".txt")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	if _, ok := file; !ok {
+		fmt.Println("Error: Empty File Content")
+		return
+	}
+
+	lines := strings.Split(strings.TrimSpace(string(file)), "\n")
+
+	antsNum, err := strconv.Atoi(lines[0])
+	if err != nil || antsNum <= 0 {
+		fmt.Println("Error: invalid ant count")
+		return
+	}
+
+	g := map[string][]string{}
+	re := regexp.MustCompile(`^\d+-\d+$`)
+
+	var start, end string
+
+	for i, l := range lines {
+		if l == "##start" {
+			if i+1 >= len(lines) || len(strings.Fields(lines[i+1])) == 0 {
+				fmt.Println("Error: invalid start")
+				return
+			}
+			start = strings.Fields(lines[i+1])[0]
+		}
+
+		if l == "##end" {
+			if i+1 >= len(lines) || len(strings.Fields(lines[i+1])) == 0 {
+				fmt.Println("Error: invalid end")
+				return
+			}
+			end = strings.Fields(lines[i+1])[0]
+		}
+
+		if re.MatchString(l) {
+			p := strings.Split(l, "-")
+
+			if len(p) != 2 || p[0] == "" || p[1] == "" {
+				fmt.Println("Error: invalid coordinate")
+				return
+			}
+
+			addEdge(g, p[0], p[1])
+		}
+	}
+
+	if start == "" || end == "" {
+		fmt.Println("Error: missing start point/ending point")
+		return
+	}
+
+	if start == end {
+		fmt.Println("Error: starting point is same as ending point")
+		return
+	}
+
+	if _, ok := g[start]; !ok {
+		fmt.Println("Error: invalid start")
+		return
+	}
+
+	if _, ok := g[end]; !ok {
+		fmt.Println("Error: invalid end")
+		return
+	}
+
+	var allPathWays [][]string
+
+	dfs(g, start, end, map[string]bool{}, []string{}, &allPathWays)
+
+	if len(allPathWays) == 0 {
+		fmt.Println("Error: no path found")
+		return
+	}
+
+	fmt.Println(allPathWays)
+
+	sort.Slice(allPathWays, func(i, j int) bool {
+		return len(allPathWays[i]) < len(allPathWays[j])
+	})
+
+	use := []Path{{Rooms: allPathWays[0]}}
+
+	best := turns(use, antsNum)
+
+	for _, p := range allPathWays[1:] {
 		ok := true
-		for _, x := range r {
-			if interceptPath(p, x) {
+
+		for _, u := range use {
+			if interceptPath(u.Rooms, p) {
 				ok = false
 				break
 			}
 		}
-		if ok {
-			r = append(r, p)
+
+		if !ok {
+			continue
+		}
+
+		tmp := append(append([]Path{}, use...), Path{Rooms: p})
+
+		if t := turns(tmp, antsNum); t < best {
+			best = t
+			use = tmp
 		}
 	}
-	return r
-}
 
-func main() {
-	b, _ := os.ReadFile("test1.txt")
-	l := strings.Split(strings.TrimSpace(string(b)), "\n")
+	assign := [][]string{}
+	tmp := make([]Path, len(use))
+	copy(tmp, use)
 
-	n, _ := strconv.Atoi(l[0])
-	re := regexp.MustCompile(`^\d+-\d+$`)
+	for range antsNum {
+		b := 0
 
-	g := map[string][]string{}
-	var s, e string
-
-	for i, v := range l {
-		if v == "##start" {
-			s = l[i+1]
-			if s != "" {
-				s = strings.Fields(l[i+1])[0]
-			} else {
-				fmt.Println("Error, empty ##start")
-				return
+		for j := range tmp {
+			if len(tmp[j].Rooms)+tmp[j].Ants < len(tmp[b].Rooms)+tmp[b].Ants {
+				b = j
 			}
 		}
-		if v == "##end" {
-			e = l[i+1]
-			if e != "" {
-				e = strings.Fields(l[i+1])[0]
-			} else {
-				fmt.Println("Error, empty ##end")
-				return
-			}
-		}
-		if re.MatchString(v) {
-			p := strings.Split(v, "-")
-			add(g, p[0], p[1])
-		}
-		if re.MatchString(v) {
-			p := strings.Split(v, "-")
-			if len(p) != 2 || p[0] == "" || p[1] == "" {
-				fmt.Println("Error: invalid coordinate line:", v)
-				return
-			}
-			add(g, p[0], p[1])
-		}
+
+		tmp[b].Ants++
+		assign = append(assign, tmp[b].Rooms)
 	}
 
-	var all []Path
-	dfs(g, s, e, map[string]bool{}, nil, &all)
+	ants := make([]Ant, antsNum)
 
-	if _, ok := g[s]; !ok {
-		fmt.Println("Error: start node not in graph")
-		return
+	for i := range ants {
+		ants[i] = Ant{i + 1, 0, assign[i]}
 	}
 
-	if _, ok := g[e]; !ok {
-		fmt.Println("rror: end node not in graph")
-		return
-	}
+	finished := 0
 
-	if len(all) < 1 {
-		fmt.Println("Error: pathway is empty")
-		return
-	}
-
-	min := len(all[0])
-	for _, p := range all {
-		if len(p) < min {
-			min = len(p)
-		}
-	}
-
-	var short []Path
-	for _, p := range all {
-		if len(p) == min {
-			short = append(short, p)
-		}
-	}
-
-	paths := pathSelection(short)
-
-	ants := []Ant{}
-	next := 1
-	done := 0
-
-	for done < n {
+	for finished < antsNum {
 		occ := map[string]bool{}
 		var out []string
 
 		for i := range ants {
 			a := &ants[i]
+
 			if a.Pos >= len(a.Path)-1 {
 				continue
 			}
-			nr := a.Path[a.Pos+1]
-			if nr != e && occ[nr] {
-				continue
-			}
-			occ[nr] = true
-			a.Pos++
-			out = append(out, fmt.Sprintf("L%d-%s", a.ID, nr))
-			if nr == e {
-				done++
-			}
-		}
 
-		for _, p := range paths {
-			if next > n {
-				break
-			}
-			if occ[p[1]] {
+			next := a.Path[a.Pos+1]
+
+			if a.Pos == 0 && occ[next] {
 				continue
 			}
-			occ[p[1]] = true
-			ants = append(ants, Ant{next, 1, p})
-			out = append(out, fmt.Sprintf("L%d-%s", next, p[1]))
-			next++
+
+			if next != end && occ[next] {
+				continue
+			}
+
+			occ[next] = true
+			a.Pos++
+
+			out = append(out, fmt.Sprintf("L%d-%s", a.ID, next))
+
+			if next == end {
+				finished++
+			}
 		}
 
 		if len(out) > 0 {
